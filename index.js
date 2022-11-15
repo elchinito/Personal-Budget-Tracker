dca411e0086542f665386750dae853dca569b3b6
elchinito <31776005+elchinito@users.noreply.github.com>
2022-11-14 19:54:26 -0800

    Added iteration to code

--- index.js
+++ index.js
@@ -1,5 +1,42 @@
-let weeklyExpenses = 0
-let monthlyExpenses = 0
-let annualExpenses = 0
+let weeklyExpenses = 0;
+let monthlyExpenses = 0;
+let annualExpenses = 0;
 
-// Your Code Here
+let i = 0;
+let j = 0;
+let k = 0;
+
+while( i < weeklyExpenseQuestions.length ){
+    let weeklyAnswer = parseFloat(window.prompt(weeklyExpenseQuestions[i]));
+    weeklyExpenses += weeklyAnswer;
+    i++;
+}
+
+while( j < monthlyExpenseQuestions.length ){
+    let monthlyAnswer = parseFloat(window.prompt(monthlyExpenseQuestions[j]));
+    monthlyExpenses += monthlyAnswer;
+    j++;
+}
+
+while( k < annualExpenseQuestions.length ){
+    let annualAnswer = parseFloat(window.prompt(annualExpenseQuestions[k]));
+    annualExpenses += annualAnswer;
+    k++;
+}
+
+/*
+for( let i = 0; i < weeklyExpenseQuestions.length; i++){
+    let weeklyAnswer = parseFloat(window.prompt(weeklyExpenseQuestions[i]));
+    weeklyExpenses += weeklyAnswer;
+}
+
+for( let j = 0; j < monthlyExpenseQuestions.length; j++){
+    let monthlyAnswer = parseFloat(window.prompt(monthlyExpenseQuestions[j]));
+    monthlyExpenses += monthlyAnswer;
+}
+
+for( let k = 0; k < annualExpenseQuestions.length; k++){
+    let annualAnswer = parseFloat(window.prompt(annualExpenseQuestions[k]));
+    annualExpenses += annualAnswer;
+}
+*/
